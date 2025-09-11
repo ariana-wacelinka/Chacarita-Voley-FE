b6ec23dbf36827b4029f05d72252f40ed3fa6a30
ariana-wacelinka <arianawacelinka@alu.frlp.un.edu.ar>
2025-09-11 11:50:39 -0300

    Archivos de configuracion base añadidos

---
+++ chacarita_voley_app/lib/app/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'theme/app_theme.dart';
+import './router.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Chacarita Voley',
+      theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
+      themeMode: ThemeMode.system,
+      routerConfig: appRouter,
+    );
+  }
+}
---
+++ chacarita_voley_app/lib/app/di.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import '../core/network/graphql_client_factory.dart';
+
+final tokenProvider = StateProvider<String?>((ref) => null);
+
+final graphQLClientProvider = Provider<GraphQLClient>((ref) {
+  final token = ref.watch(tokenProvider);
+  return GraphQLClientFactory.create(
+    baseUrl: '',
+    //Cuando Lu tenga el back arriba agregar la variable de entorno y reemplazar.
+    token: token,
+  );
+});
