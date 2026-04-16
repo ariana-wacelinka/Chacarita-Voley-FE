501e59b505a04ac496719522e4629d020c641279
Ariana Wacelinka <arianawacelinka@alu.frlp.utn.edu.ar>
2026-04-16 16:12:59 -0300

    ↩️ Mejorar back del sistema en paginas wrapper

--- chacarita_voley_app/lib/app/layout/page_wrapper.dart
+++ chacarita_voley_app/lib/app/layout/page_wrapper.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 /// Wrapper para páginas fuera del ShellRoute que maneja el botón back correctamente
 class PageWrapper extends StatelessWidget {
   final Widget child;
+  final String fallbackRoute;
 
-  const PageWrapper({super.key, required this.child});
+  const PageWrapper({
+    super.key,
+    required this.child,
+    this.fallbackRoute = '/home',
+  });
 
   @override
   Widget build(BuildContext context) {
-    return child;
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (didPop, result) {
+        if (didPop) return;
+        if (context.canPop()) {
+          context.pop();
+        } else {
+          context.go(fallbackRoute);
+        }
+      },
+      child: child,
+    );
   }
 }
